3bee75bb10899c8f98f748ebd11f6c7eb8607bae
Hugo Pinto <hugo.lcpinto@gmail.com>
2018-01-25 00:54:52 -0200

    Updated interfaces

--- app/src/main/java/com/ihealthy/ihealthy/HistoryFragment.java
+++ app/src/main/java/com/ihealthy/ihealthy/HistoryFragment.java
@@ -33,6 +33,7 @@ public class HistoryFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view  =inflater.inflate(R.layout.fragment_history, container, false);
+        getActivity().setTitle("My history");
 
         fillHistoryValues(view);
 
--- app/src/main/java/com/ihealthy/ihealthy/RestaurantViewActivity.java
+++ app/src/main/java/com/ihealthy/ihealthy/RestaurantViewActivity.java
@@ -1,20 +1,32 @@
 package com.ihealthy.ihealthy;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RestaurantViewActivity extends AppCompatActivity {
 
     private Intent intent;
+    private String restaurantName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +39,7 @@ public class RestaurantViewActivity extends AppCompatActivity {
 
         intent = getIntent();
 
-        String restaurantName = intent.getExtras().getString("restaurantName");
+        restaurantName = intent.getExtras().getString("restaurantName");
 
         setTitle(restaurantName);//Seta o título da activity
 
@@ -37,14 +49,32 @@ public class RestaurantViewActivity extends AppCompatActivity {
 
     private void loadMenuList(){
 
-        final String [] menuList = getMenuInfo();
+        final ArrayList<MenuItem> menuList = getMenuInfo();
         ListView listView = findViewById(R.id.menu_list_view);
 
-        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<>(
+        ArrayAdapter listViewAdapter = new ArrayAdapter(
                 this,
-                android.R.layout.simple_list_item_1,
+                R.layout.restaurant_menu_item,
+                android.R.id.text1,
                 menuList
-        );
+        ) {
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                LayoutInflater inflater = (LayoutInflater) getContext()
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View view = inflater.inflate(R.layout.restaurant_menu_item, parent, false);
+
+                TextView text1 = (TextView) view.findViewById(R.id.text1);
+                TextView text2 = (TextView) view.findViewById(R.id.text2);
+                Button button = (Button) view.findViewById(R.id.button1);
+
+                text1.setText(menuList.get(position).getName());
+                text2.setText("Calories: "+menuList.get(position).getCalories());
+                button.setText("R$"+menuList.get(position).getPrice());
+                return view;
+            }
+        };
 
         listView.setAdapter(listViewAdapter);
 
@@ -57,13 +87,77 @@ public class RestaurantViewActivity extends AppCompatActivity {
         });
     }
 
-    private String [] getMenuInfo(){
-        String[] menuList = {
-                "Fish",
-                "Meat",
-                "Rice"
-        };
+    private ArrayList<MenuItem> getMenuInfo(){
 
-        return menuList;
+        ArrayList<MenuItem> menuItems = new ArrayList<>();
+
+        //Criando objeto de busca
+        final ParseQuery<ParseObject> query = ParseQuery.getQuery("Restaurant");
+        query.whereEqualTo("name", restaurantName);
+
+        try {
+
+            //busca na web a lista de resultados da querry
+            List<ParseObject> objects = query.find();
+
+            if(objects.size()>0) {
+                final ParseQuery<ParseObject> query1 = ParseQuery.getQuery("Food");
+                query1.whereEqualTo("fromRestaurant", ParseObject.createWithoutData("Restaurant", objects.get(0).getObjectId()));
+
+                List<ParseObject> objects1 = query1.find();
+                for (int i = 0; i < objects1.size(); i++){
+                    MenuItem item = new MenuItem();
+                    item.setName(objects1.get(i).getString("foodname"));
+                    item.setCalories(objects1.get(i).getString("calories"));
+                    item.setPrice(objects1.get(i).getInt("price"));
+                    item.setIngredients(objects1.get(i).getString("foodname"));
+                    menuItems.add(item);
+                }
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return menuItems;
+    }
+
+    public class MenuItem {
+        private String name;
+        private String ingredients;
+        private String calories;
+        private float price;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getIngredients() {
+            return ingredients;
+        }
+
+        public void setIngredients(String ingredients) {
+            this.ingredients = ingredients;
+        }
+
+        public String getCalories() {
+            return calories;
+        }
+
+        public void setCalories(String calories) {
+            this.calories = calories;
+        }
+
+        public float getPrice() {
+            return price;
+        }
+
+        public void setPrice(float price) {
+            this.price = price;
+        }
     }
 }
