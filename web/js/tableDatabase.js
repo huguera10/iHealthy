7aed0525bf2dd9884c9b60bbd1153153ec78e9a0
ggaribeiro <ggaribeiro@hotmail.com>
2018-01-27 14:45:23 -0200

    Adiciona a comunicaÃÃo com o banco no pagina de gerencia de restaurante

--- web/js/tableDatabase.js
+++ web/js/tableDatabase.js
@@ -8,84 +8,86 @@ Parse.initialize("TQhl7DbLKccsTnRgMR6rtlCSWgo7KTdfGfGDlx32", "q6yDi6SBuC3LhL8ohX
 Parse.serverURL = "https://parseapi.back4app.com/";
 
 var currentUser = Parse.User.current();
-var restaurant = "";
-//var restaurant;
+var restaurantObject;
+
 if (!currentUser) {
-// do stuff with the user
+
     window.location = "index.html";
 }
 
 var query = new Parse.Query("Restaurant");
 query.equalTo('owner', Parse.User.current());
 query.find({
-  success: function(results) {
-    alert("Successfully retrieved " + results.length + " id");
-    // Do something with the returned Parse.Object values
-    for (var i = 0; i < results.length; i++) {
-      restaurant = results[i].get("name");
-      alert(object.id + ' - ' + object.get('name'));
-    }
-  },
-  error: function(error) {
-    alert("Error: " + error.code + " " + error.message);
-  }
-});
-
-function logout() {
-    Parse.User.logOut().then(() => {
-        window.location = "index.html";
-    });
-}
-
-$(document).ready(function ($) {
-    var random_id = function () {
-        var id_num = Math.random().toString(9).substr(2, 3);
-        var id_str = Math.random().toString(36).substr(2);
-        return id_num + id_str;
-    };
-    
-    
-    var table = '';
-    table += '<h2><div class="restaurant_name" edit_type="click" col_name="fname">' + restaurant + '</div>';
-    table += '<span class="btn_edit_name"> <a href="#" class="btn btn-link">Editar</a> </span>';
-    table += '<span class="btn_save_name"> <a href="#" class="btn btn-link">Salvar</a> | </span>';
-    table += '<span class="btn_cancel_name"> <a href="#" class="btn btn-link"> Cancelar </a> </span></h2>';
-    table += '<div class="row">';
-    table += '<div class="col-xs-12">';
-    table += '<table class="table table-hover">';
-    table += '<thead>';
-    table += '<tr>';
-    table += '<th>Nome</th>';
-    table += '<th>Ingredientes</th>';
-    table += '<th>Calorias</th>';
-    table += '<th>Preço</th>';
-    table += '<th>Gerenciar</th> </tr> </thead>';
-    for (var i = 0, max = 4; i < max; i++) {
-        var row_id = random_id();
-        table += '<tr row_id="' + row_id + '">';
-        for (var j = 0, max = 4; j < max; j++) {
-            table += '<td><div class="row_data" edit_type="click" col_name="fname">' + (j * i) + '</div>' + '</td>';
+    success: function (results) {
+        for (var i = 0; i < results.length; i++) {
+            restaurantObject = results[i];
         }
 
-        //Editar ---> Inicio
-        table += '<td>';
-        table += '<span class="btn_edit"> <a href="#" class="btn btn-link" row_id="' + row_id + '"> Editar</a> </span>';
-        //Só aparece quando o botão editar é clikado
-        table += '<span class="btn_save"> <a href="#" class="btn btn-link" row_id="' + row_id + '"> Salvar</a> | </span>';
-        table += '<span class="btn_cancel"> <a href="#" class="btn btn-link" row_id="' + row_id + '"> Cancelar</a> </span>';
-        table += '</td>';
-        //Editar ---> Fim
-        table += '</tr>';
-    }
+        var query = new Parse.Query("Food");
+        query.equalTo("fromRestaurant", restaurantObject);
 
-    table += '</table>';
-    table += '</div>';
-    table += '</div>';
-    $('.tbl_user_data').html(table);
-    $('.btn_save').hide();
-    $('.btn_cancel').hide();
-    $('.btn_save_name').hide();
-    $('.btn_cancel_name').hide();
+        query.find({
+            success: function (results) {
+
+                var table = '';
+                table += '<h2><div id="restaurant" class="restaurant_name" edit_type="click" col_name="fname">' + restaurantObject.get('name') + '</div>';
+                table += '<span class="btn_edit_name"> <a href="#" class="btn btn-link">Editar</a> </span>';
+                table += '<span class="btn_save_name"> <a href="#" class="btn btn-link">Salvar</a> | </span>';
+                table += '<span class="btn_cancel_name"> <a href="#" class="btn btn-link"> Cancelar </a> </span></h2>';
+                table += '<div class="row">';
+                table += '<div class="col-xs-12">';
+                table += '<table class="table table-hover">';
+                table += '<thead>';
+                table += '<tr>';
+                table += '<th>Nome</th>';
+                table += '<th>Ingredientes</th>';
+                table += '<th>Calorias</th>';
+                table += '<th>Preço</th>';
+                table += '<th>Gerenciar</th> </tr> </thead>';
+
+                for (var i = 0; i < results.length; i++) {
+                    var object = results[i];
+
+                    table += '<tr row_id="' + object.id + '">';
+
+                    table += '<td><div class="row_data" edit_type="click" col_name="fname">' + object.get('foodname') + '</div>' + '</td>';
+                    table += '<td><div class="row_data" edit_type="click" col_name="fname">' + object.get('ingredients') + '</div>' + '</td>';
+                    table += '<td><div class="row_data" edit_type="click" col_name="fname">' + object.get('calories') + '</div>' + '</td>';
+                    table += '<td><div class="row_data" edit_type="click" col_name="fname">' + object.get('price') + '</div>' + '</td>';
+
+                    //Editar ---> Inicio
+                    table += '<td>';
+                    table += '<span class="btn_edit"> <a href="#" class="btn btn-link" row_id="' + object.id + '"> Editar</a> </span>';
+                    //Só aparece quando o botão editar é clikado
+                    table += '<span class="btn_save"> <a href="#" class="btn btn-link" row_id="' + object.id + '"> Salvar</a> | </span>';
+                    table += '<span class="btn_cancel"> <a href="#" class="btn btn-link" row_id="' + object.id + '"> Cancelar</a> </span>';
+                    table += '</td>';
+                    //Editar ---> Fim
+                    table += '</tr>';
+                }
+
+                table += '</table>';
+                table += '</div>';
+                table += '</div>';
+                $('.tbl_user_data').html(table);
+                $('.btn_save').hide();
+                $('.btn_cancel').hide();
+                $('.btn_save_name').hide();
+                $('.btn_cancel_name').hide();
+            },
+            error: function (error) {
+                alert("Error: " + error.code + " " + error.message);
+            }
+        });
+    },
+    error: function (error) {
+        alert("Error: " + error.code + " " + error.message);
+    }
+});
+
+$(document).ready(function ($) {
+
+
 
     //Botão Editar <----> Inicio
     $(document).on('click', '.btn_edit', function (event) {
@@ -118,8 +120,6 @@ $(document).ready(function ($) {
 
         event.preventDefault();
         var name_title = $(this).closest('h2');
-
-        //var row_id = tbl_row.attr('row_id');
 
         name_title.find('.btn_save_name').show();
         name_title.find('.btn_cancel_name').show();
@@ -168,7 +168,6 @@ $(document).ready(function ($) {
         event.preventDefault();
 
         var name_title = $(this).closest('h2');
-        //var row_id = tbl_row.attr('row_id');
 
         name_title.find('.btn_save_name').hide();
         name_title.find('.btn_cancel_name').hide();
@@ -209,20 +208,29 @@ $(document).ready(function ($) {
                 .css('padding', '');
 
         //--->get row data > start
-        var arr = {};
+        var Food = Parse.Object.extend("Food");
+        var object = new Food();
+
+        object.set("objectId", row_id);
         tbl_row.find('.row_data').each(function (index, val)
         {
-            var col_name = $(this).attr('col_name');
-            var col_val = $(this).html();
-            arr[col_name] = col_val;
+            var col_val = $(this).text();
+            if (index == 0) {
+                object.set("foodname", col_val);
+            }
+            if (index == 1) {
+                object.set("ingredients", col_val);
+            }
+            if (index == 2) {
+                object.set("calories", col_val);
+            }
+            if (index == 3) {
+                object.set("price", parseFloat(col_val));
+            }
         });
+
+        object.save();
         //--->get row data > end
-
-        //use the "arr"	object for your ajax call
-        $.extend(arr, {row_id: row_id});
-
-        //out put to show
-        $('.post_msg').html('<pre class="bg-success">' + JSON.stringify(arr, null, 2) + '</pre>');
     });
     //Botão Salvar <----> Fim
     //
@@ -232,7 +240,6 @@ $(document).ready(function ($) {
         event.preventDefault();
 
         var name_title = $(this).closest('h2');
-        //var row_id = tbl_row.attr('row_id');
 
         name_title.find('.btn_save_name').hide();
         name_title.find('.btn_cancel_name').hide();
@@ -248,22 +255,24 @@ $(document).ready(function ($) {
                 .css('padding', '');
 
         //--->get row data > start
-        //var arr = {};
         name_title.find('.restaurant_name').each(function (index, val)
         {
-            var col_name = $(this).attr('col_name');
-            var col_val = $(this).html();
-            //arr[col_name] = col_val;
+            var col_val = $(this).text();
+
+            //Salvando no banco
+            restaurantObject.set("name", col_val);
+            restaurantObject.save();
+
         });
         //--->get row data > end
-
-        //use the "arr"	object for your ajax call
-        //$.extend(arr, {row_id: row_id});
-
-        //out put to show
-        //$('.post_msg').html('<pre class="bg-success">' + JSON.stringify(arr, null, 2) + '</pre>');
     });
     //Botão Salvar Nome Restaurante <----> Fim
 
 });
+
+function logout() {
+    Parse.User.logOut().then(() => {
+        window.location = "index.html";
+    });
+}
 
